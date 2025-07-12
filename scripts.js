9bfa7817875e9ec56010ba6d105efd4b10502532
lucasrossic <lucas1rc2003@gmail.com>
2025-07-12 20:42:05 -0300

    Preentrega

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-// Oculta la top-bar y hace que la navbar suba suavemente al hacer scroll
 window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.navbar');
-  const topBar = document.querySelector('.top-bar');
+  const wrapper = document.querySelector('.topnav-wrapper');
 
-  const hasScrolled = window.scrollY > 50;
-
-  topBar.classList.toggle('hidden', hasScrolled);
-  navbar.classList.toggle('scrolled', hasScrolled);
+  if (window.scrollY > 50) {
+    wrapper.classList.add('scrolled');
+  } else {
+    wrapper.classList.remove('scrolled');
+  }
 });
